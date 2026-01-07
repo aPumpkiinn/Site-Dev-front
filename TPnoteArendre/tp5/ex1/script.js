c51a2a0e68d76c50fd738196c29f2866f493a649
aPumpkiinn <156137186+aPumpkiinn@users.noreply.github.com>
2026-01-08 00:02:01 +0100

    Delete TPnoteArendre directory

--- TPnoteArendre/tp5/ex1/script.js
+++
@@ -1,8 +0,0 @@
-const boutonRetour = document.querySelector('.btn-retour');
-boutonRetour.addEventListener('click', function(event) {
-    event.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
